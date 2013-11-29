124622f2626e7a9c5470bd81ae72c0ce91c7328f
Rui Yang <rui.yang@myob.com>
2013-11-29 13:36:28 +1100

    remove default shell buff loading. using revive to do that

--- users/init.el
+++ users/init.el
@@ -1,7 +1,7 @@
-(let ((default-directory "~/liveaccounts/frontend/"))
-  (shell "ruby"))
-(let ((default-directory "~/liveaccounts/"))
-  (shell "1"))
+;; (let ((default-directory "~/liveaccounts/frontend/"))
+;;   (shell "ruby"))
+;; (let ((default-directory "~/liveaccounts/"))
+;;   (shell "1"))
 
 (add-to-list 'auto-mode-alist '("buildfile" . ruby-mode))
 (set-default 'truncate-lines nil)
