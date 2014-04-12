49204708bb482a8617c9ac92688c876675ace2a0
ryang <ryang.sei@gmail.com>
2014-04-12 23:15:53 +1000

    org mode config

---
+++ setup-org.el
@@ -0,0 +1,23 @@
+;;(add-to-list 'load-path (expand-file-name "org-mode/lisp" user-emacs-directory))
+;;(add-to-list 'load-path (expand-file-name "org-mode/contrib/lisp" user-emacs-directory))
+
+(org-babel-do-load-languages
+ 'org-babel-load-languages
+ '((emacs-lisp . t)
+   (clojure . t)))
+
+;; Use cider as the clojure execution backend
+(setq org-babel-clojure-backend 'cider)
+
+;; Let's have pretty source code blocks
+(setq org-edit-src-content-indentation 0
+      org-src-tab-acts-natively t
+      org-src-fontify-natively t
+      org-confirm-babel-evaluate nil)
+
+(add-hook 'org-shiftup-final-hook 'windmove-up)
+(add-hook 'org-shiftleft-final-hook 'windmove-left)
+(add-hook 'org-shiftdown-final-hook 'windmove-down)
+(add-hook 'org-shiftright-final-hook 'windmove-right)
+
+(provide 'setup-org)
