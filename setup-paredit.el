9359cc6600b6994ccb18bbccc787fdac6def4da3
Rui Yang <rui.yang@myob.com>
2014-10-23 16:46:42 +1100

    install evil mode

--- setup-paredit.el
+++ setup-paredit.el
@@ -27,9 +27,9 @@
       (kill-region (region-beginning) (region-end))
     (paredit-backward-kill-word)))
 
-(add-hook 'clojure-mode-hook (lambda () (paredit-mode 1)))
-(add-hook 'nrepl-mode-hook (lambda () (paredit-mode 1)))
-(add-hook 'emacs-lisp-mode-hook (lambda () (paredit-mode 1)))
+;; (add-hook 'clojure-mode-hook (lambda () (paredit-mode 1)))
+;; (add-hook 'nrepl-mode-hook (lambda () (paredit-mode 1)))
+;; (add-hook 'emacs-lisp-mode-hook (lambda () (paredit-mode 1)))
 
 (define-key paredit-mode-map (kbd "M-(") 'paredit-wrap-round)
 (define-key paredit-mode-map (kbd "M-)") 'paredit-wrap-round-from-behind)
