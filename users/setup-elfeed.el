9cdb0d7d729fe398092bf19a355a4b7838109b3f
Rui Yang <rui.yang@myob.com>
2014-12-11 14:00:18 +1100

    change in elfeed

--- users/setup-elfeed.el
+++ users/setup-elfeed.el
@@ -4,7 +4,6 @@
         ("http://blog.guillermowinkler.com/atom.xml" Tech)
         ("http://feed.codeofrob.com/RobAshton" Tech)
         ("http://blog.8thlight.com/feed/atom.xml" Tech Bob)
-        ("http://www.oursteps.com.au/bbs/forum.php?mod=rss&fid=42&auth=bd38BYfK2azQa92yMFonK6A5ihTjDkvbNzsWytdFdaP4BmoRdhmzT3lSrKB0nw" Portal Job)
         ("http://www.oursteps.com.au/bbs/forum.php?mod=rss&fid=89&auth=3f702nWzBrApV5bHyKdRBOPcQJqHLck4M5lXD%2F0W%2BG0mOJT7rUWeGhMaCFvIBQ" Portal Shop)
         ))
 
