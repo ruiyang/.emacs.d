2f6f5c66a4f90ee6f3a6f85126a48800aaaa987f
Rui Yang <ryang.sei@gmail.com>
2015-01-08 13:47:35 +1100

    fix keybinding error in setup-smartparens

--- users/setup-smartparens.el
+++ users/setup-smartparens.el
@@ -28,7 +28,7 @@
 (define-key sp-keymap (kbd "M-<backspace>") 'sp-unwrap-sexp)
 (define-key sp-keymap (kbd "M-<DEL>") 'sp-backward-unwrap-sexp)
 
-(define-key sp-keymap (kbd "C-<right>" 'sp-forward-slurp-sexp))
+(define-key sp-keymap (kbd "C-<right>") 'sp-forward-slurp-sexp)
 (define-key sp-keymap (kbd "C-<left>") 'sp-forward-barf-sexp)
 (define-key sp-keymap (kbd "C-M-<left>") 'sp-backward-slurp-sexp)
 (define-key sp-keymap (kbd "C-M-<right>") 'sp-backward-barf-sexp)
