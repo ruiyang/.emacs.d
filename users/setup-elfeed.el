220b727f3231a8fd6418dcc621ba7397bf5eda90
Rui Yang <ryang.sei@gmail.com>
2015-01-21 11:30:57 +1100

    elfeed changes

--- users/setup-elfeed.el
+++ users/setup-elfeed.el
@@ -1,10 +1,10 @@
 (setq elfeed-feeds
-      '(("http://martinfowler.com/feed.atom" Tech)
-        ("http://feeds.feedburner.com/SanityInc" Tech)
-        ("http://blog.guillermowinkler.com/atom.xml" Tech)
-        ("http://feed.codeofrob.com/RobAshton" Tech)
-        ("http://blog.8thlight.com/feed/atom.xml" Tech Bob)
-        ("http://www.oursteps.com.au/bbs/forum.php?mod=rss&fid=89&auth=3f702nWzBrApV5bHyKdRBOPcQJqHLck4M5lXD%2F0W%2BG0mOJT7rUWeGhMaCFvIBQ" Portal Shop)
+      '(("http://martinfowler.com/feed.atom" tech)
+        ("http://feeds.feedburner.com/SanityInc" tech)
+        ("http://blog.guillermowinkler.com/atom.xml" tech)
+        ("http://feed.codeofrob.com/RobAshton" tech)
+        ("http://blog.8thlight.com/feed/atom.xml" tech bob)
+        ("http://www.oursteps.com.au/bbs/forum.php?mod=rss&fid=89&auth=3f702nWzBrApV5bHyKdRBOPcQJqHLck4M5lXD%2F0W%2BG0mOJT7rUWeGhMaCFvIBQ" portal shop)
         ))
 
 (provide 'setup-elfeed)
